94d427353249d622e5d0d1438bf5b0eb209d751e
Mohamed Doffer Mohamed Nasir <73598690+mohamed-nasir@users.noreply.github.com>
2021-06-04 18:09:06 +0800

    v0.2.9

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,41 @@
+export const links = [
+  {
+    label: 'Exchange',
+    icon: 'HomeIcon',
+    href:
+      'https://swap.cleanocean.io/#/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0x579F11C75Eb4e47F5290122e87CA411644aDCD97',
+  },
+  {
+    label: 'Liquidity',
+    icon: 'FarmIcon',
+    href:
+      'https://swap.cleanocean.io/#/add/0xe9e7cea3dedca5984780bafc599bd69add087d56/0x579F11C75Eb4e47F5290122e87CA411644aDCD97',
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/CleanOcean",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/CleanOceanANN",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/CleanOcean",
+  },
+];
+
+export const MENU_HEIGHT = 130;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
---
+++ src/widgets/Menu/index.ts
@@ -0,0 +1,4 @@
+export { default as Menu } from "./Menu";
+export { default as CakePrice } from "./components/CakePrice";
+export { links as menuConfig } from "./config";
+export type { NavProps, LangType, MenuEntry } from "./types";
